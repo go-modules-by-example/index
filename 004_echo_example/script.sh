22670ffe91f6fc86853d36481ca42778637ff8d5
Paul Jolly <paul@myitcv.io>
2018-05-31 06:50:25 +0100

    Initial commit of echo example (#7)

---
+++ 004_echo_example/script.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+set -u
+set -x
+
+assert()
+{
+  E_PARAM_ERR=98
+  E_ASSERT_FAILED=99
+
+  if [ -z "$2" ]
+  then
+    exit $E_PARAM_ERR
+  fi
+
+  lineno=$2
+
+  if [ ! $1 ]
+  then
+    echo "Assertion failed:  \"$1\""
+    echo "File \"$0\", line $lineno"
+    exit $E_ASSERT_FAILED
+  fi
+}
+
+ensure_github_repo()
+{
+	# TODO improve this
+	cat <<EOD | curl -s -H "Content-Type: application/json" --request POST -d @- https://api.github.com/user/repos > /dev/null
+{
+  "name": "$1"
+}
+EOD
+
+	curl -s -H "Content-Type: application/json" https://api.github.com/repos/$GITHUB_USERNAME/$1 | grep -q '"id"'
+}
+
+# **START**
+
+export GOPATH=$HOME
+export PATH=$GOPATH/bin:$PATH
+echo "machine github.com login $GITHUB_USERNAME password $GITHUB_PAT" >> $HOME/.netrc
+echo "" >> $HOME/.netrc
+echo "machine api.github.com login $GITHUB_USERNAME password $GITHUB_PAT" >> $HOME/.netrc
+git config --global user.email "$GITHUB_USERNAME@example.com"
+git config --global user.name "$GITHUB_USERNAME"
+
+# block: go get vgo
+go get -u golang.org/x/vgo
+assert "$? -eq 0" $LINENO
+
+# block: step 0
+mkdir hello
+cd hello
+
+# block: step 1
+cat <<EOD > main.go
+package main // import "example.com/hello"
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+EOD
+echo > go.mod
+
+# block: step 2
+vgo get github.com/labstack/echo@v3.2.1
+assert "$? -eq 0" $LINENO
+
+# block: step 3
+vgo build
+assert "$? -eq 0" $LINENO
+cat go.mod
+
+# block: version details
+vgo version
+echo "vgo commit: $(command cd $(go list -f "{{.Dir}}" golang.org/x/vgo); git rev-parse HEAD)"
