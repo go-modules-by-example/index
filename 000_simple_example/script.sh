53512cb0675f420e75c46aa98fb1fb801f79da3a
Paul Jolly <paul@myitcv.io>
2018-10-19 22:02:03 +0100

    000_simple_example: add simple example for main README (#75)

---
+++ 000_simple_example/script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# **START**
+
+export GOPATH=$HOME
+export PATH=$GOPATH/bin:$PATH
+echo "machine github.com login $GITHUB_USERNAME password $GITHUB_PAT" >> $HOME/.netrc
+echo "" >> $HOME/.netrc
+echo "machine api.github.com login $GITHUB_USERNAME password $GITHUB_PAT" >> $HOME/.netrc
+git config --global user.email "$GITHUB_USERNAME@example.com"
+git config --global user.name "$GITHUB_USERNAME"
+git config --global advice.detachedHead false
+git config --global push.default current
+
+# block: script
+cd $HOME
+mkdir simple
+cd simple
+go mod init example.com/simple
+go list -m
